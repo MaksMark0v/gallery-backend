822aafbd417f31a620cb21c2d60684da764ebac1
vvoloshchuk <volodymyr.voloshchuk@eliftech.com>
2024-09-27 17:29:51 +0300

    added random numbers

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import http from 'http';
+import getRandomInt from './helpers/random-numbers.js';
 
 const server = http.createServer((req, res) => {
+  const randomNumber = getRandomInt(1, 100);
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World YAY\n From nodejs\n');
+  res.end(`Hello World YAY\nFrom nodejs\n${randomNumber}`);
 });
 
-server.listen(3000, () => {
-    console.log('Server running at http://localhost:5002/');
+server.listen(5001, () => {
+    console.log('Server running at http://localhost:5001/');
 });
