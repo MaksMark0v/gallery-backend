cd46d9cc4a883fba4d78776cf890a4fdd6064d5e
MaksMark0v <168219284+MaksMark0v@users.noreply.github.com>
2024-09-27 17:04:19 +0300

    fixed modules & port

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const server = http.createServer ((req, res) =>{
     res.end('Hello World!\n');
 });
 
-server.listen(3000, () => {
+server.listen(5002, () => {
     console.log('Server is running at http://localhost:5002/');
 });
