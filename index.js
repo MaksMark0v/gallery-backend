c8e6185ba333e0c4cda47a70cc131db8d29b75ff
MaksMark0v <168219284+MaksMark0v@users.noreply.github.com>
2024-09-29 17:24:24 +0300

    random-numbers

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import http from 'http';
+import getRandomInt from './helpers/random-numbers';
 
 const server = http.createServer ((req, res) =>{
+    const randomNumber = getRandomInt(1, 100);
     res.writeHead (200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World!\n');
+    res.end(`Hello World!\nFrom nodejs\n${randomNumber}`);
 });
 
 server.listen(5002, () => {
