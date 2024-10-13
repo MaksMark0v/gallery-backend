8821ea16608dd1c337ee68cd7f1b81719d99c25d
MaksMark0v <168219284+MaksMark0v@users.noreply.github.com>
2024-10-13 15:32:06 +0300

    worked on the function of fetching user list

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,22 +1,36 @@
 
 import path from 'path';
 
-import bodyParser from 'body-parser'; // Імпорт модуля body-parser для парсингу JSON-запитів
 import getRandomInt from '../helpers/random-numbers.js'; // Імпорт функції для генерації випадкових чисел
 
 import router from '../router/index.js'
+import { getUsersData } from '../reposetory/userRepo.js';
 
 const __dirname = path.resolve();
 
 // Роут для головної сторінки
 router.get('/', (req, res) => {
     res.setHeader('Content-Type', 'text/html'); // Встановлення заголовка Content-Type
-    res.send(`Hello World YAY <br> From express <br> <a href="/page">${getRandomInt(1, 100)}</a>`); // Відправка відповіді з випадковим числом
+    res.send(`Hello World YAY <br> From express <br> <a href="/user">${getRandomInt(1, 100)}</a>`); // Відправка відповіді з випадковим числом
 });
 
 router.get('/page', (req, res) => {
     const pathToResourses = `${__dirname}/resourses/`;
     res.sendFile(pathToResourses, 'index.html');
+});
+
+router.get('/user', async (req, res) => {
+    try {
+        console.log('req: ', req.query);
+
+        const usersData = await getUsersData(req.query);
+        res.send(usersData);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(`Internal server error: ${error.message}`);
+    }
+
+
 });
 
 // Роут для користувача за ID
@@ -25,10 +39,4 @@ router.get('/user/:id', function (req, res) {
     res.send(`hello, user! ${userId}`); // Відправка відповіді з ID користувача
 });
 
-// Роут для створення нового користувача
-router.post('/user', bodyParser.json(), function (req, res) {
-    const newUser = req.body; // Отримання даних нового користувача з тіла запиту
-    const userData = JSON.stringify(newUser); // Перетворення даних користувача у JSON-рядок
-    res.send(`New, user created! ${userData}`); // Відправка відповіді з даними нового користувача
-});
 export default router;
